b4fcb0e26da7b1ca15fd5ce1d07988fd3de05f3a
Andrew Talley <andrew.d.talley@vanderbilt.edu>
2020-10-24 12:08:38 -0500

    Fix

--- pages/code-fix.tsx
+++ pages/code-fix.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 
 export default () => {
-  const code = parseInt(sessionStorage.getItem("code"));
+  const storage = typeof sessionStorage === "undefined" ? null : sessionStorage;
+  const code = parseInt(storage?.getItem("code"));
 
   useEffect(() => {
-    sessionStorage.removeItem("code");
-    sessionStorage.setItem("code", "1776");
+    storage?.removeItem("code");
+    storage?.setItem("code", "1776");
   });
 
   return code === 1776 ? <span>Done!</span> : <span>Working...</span>;
